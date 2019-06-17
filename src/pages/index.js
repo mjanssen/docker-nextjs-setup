e3078af4d780bc7ebf7a9c807c6a86f830565758
mjanssen <dev.marnix@gmail.com>
2019-06-17 18:27:30 +0200

    Added composed with backend base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,33 @@
+import { useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+
 function Home() {
+  fetch('http://backend:3004', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      variables: {},
+      operationName: '',
+      query: `
+      query Joke {
+        joke {
+          type
+          value {
+            id
+            joke
+          }
+        }
+      }
+      `,
+    }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log('data', data);
+    });
+
   return <div>Welcome to Next.js!</div>;
 }
 
